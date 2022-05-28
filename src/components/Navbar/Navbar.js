2deb6f318c653a6ab0b3fa62ad014a7e481ea715
Richa Maharjan <richoo234@gmail.com>
2022-05-28 18:10:19 +0545

    Navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Navbar.css'
+import { NavbarItems } from "./NavbarItem";
+function Navbar(){
+    return(
+        <div className="Topbar">
+            <div className="TopbarLeft">
+                <img  src="assets/images/RentSahayak.png"></img>
+            </div>
+            <ul className="TopbarList">
+                {NavbarItems.map((items,index)=>{
+                    return(
+
+                    <li   key={index}>
+                        <a className={items.cName} href={items.url}>{items.title}</a>
+                    </li>
+                    )
+
+                })}
+            </ul>
+            
+            
+        </div>
+    )
+
+}
+export default Navbar;
---
+++ src/components/Navbar/NavbarItem.js
@@ -0,0 +1,39 @@
+export const NavbarItems=[
+    {
+
+        title:"About",
+        
+        url:'#',
+        cName:'sidebarListItemText'
+    },
+    {
+        title:"Categories",
+        
+        url:'#',
+        cName:'sidebarListItemText'
+    },
+    {
+        title:"Our Team",
+        
+        url:'#',
+        cName:'sidebarListItemText'
+    },
+    {
+        title:"Contact Us",
+       
+        url:'#',
+        cName:'sidebarListItemText'
+    },
+    {
+        title:"Login",
+       
+        url:'#',
+        cName:'sidebarListItemText'
+    },
+    {
+        title:"Register",
+       
+        url:'#',
+        cName:'sidebarListItemText'
+    }
+]
